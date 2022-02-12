acca331062ce3280909d2fe15b55c77d53b56f98
Bekhruz Kasymov <52739854+behaqos@users.noreply.github.com>
2022-02-12 20:40:58 +0300

    doing 04

--- level02/Ressources/pseudo_code.c
+++ level02/Ressources/pseudo_code.c
@@ -1,18 +1,18 @@
 int main(void){
     char username[96];
-    char password_file[40];
+    char password_text[40];
     char password[96];
     bzero(username, 96);
-    bzero(password_file, 40);
+    bzero(password_text, 40);
     bzero(password, 96);
     FILE *fd = fopen("/home/users/level03/.pass", "r");
     if (!fd){
         fwrite("ERROR: failed to open password file\n", 0x1, 0x24, stderr);
         exit(1);
     }
-    int res = fread(password_file, 0x1, 41, fd);
-    int n = strcsnpn(password_file, "\n");
-    password_file[n] = '\0';
+    int res = fread(password_text, 0x1, 41, fd);
+    int n = strcsnpn(password_text, "\n");
+    password_text[n] = '\0';
     if (res != 41){
         fwrite("ERROR: failed to read password file\n", 0x1, 0x24, stderr);
         fwrite("ERROR: failed to read password file\n", 0x1, 0x24, stderr);
@@ -34,7 +34,7 @@ int main(void){
     n = strcsnpn(password, "\n");
     password[n] = '\0';
     puts("\\**************************************/");
-    if(strncmp(password_file, password, 41) != 0){
+    if(strncmp(password_text, password, 41) != 0){
         printf(username);
         puts(" does not have access!");
         exit(1);
