554372ef309d0bdcf68081ed16f8dac796a77f25
Bekhruz Kasymov <52739854+behaqos@users.noreply.github.com>
2022-02-13 14:38:09 +0300

    doing 05

---
+++ level04/Ressources/pseudo_code.c
@@ -0,0 +1,30 @@
+int main(void)
+{
+  int   status;
+  pid_t pid; //- структура, которая хранит номер процесса.
+  char  shellcode[32];
+
+  pid = fork();
+  if (pid == 0)
+  {
+    prctl(0x1, 0x1);
+    ptrace(PTRACE_TRACEME, 0, 0, 0);
+    puts("Give me some shellcode, k");
+    gets(shellcode);
+  }
+  else
+  {
+    do
+    {
+      wait(&status);
+      if ((status & 0x7f) == 0)
+      {
+        puts("child is exiting...");
+        return 0;
+      }
+    } while (ptrace(PTRACE_PEEKUSER, pid, 0x2c, 0) != 0xb)
+    puts("no exec() for you");
+    kill(pid, 0x9);
+  }
+  return (0);
+}
